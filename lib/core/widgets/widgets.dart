c6872c7afe915be2b342c5a9ce91317d4153fe9c
amin.khademi <120728947+amin-khademi@users.noreply.github.com>
2025-02-27 14:37:02 +0330

    🔨 refactor: import error

--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -1,14 +1,12 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shimmer/shimmer.dart';
 
-part 'image.dart';
 part 'delay.dart';
+part 'image.dart';
 part 'shimmer.dart';
-
